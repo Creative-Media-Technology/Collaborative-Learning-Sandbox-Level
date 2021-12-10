e70448adcf6fc2c7559731822182ae655484d5cc
Mr-Middleton <80347860+Mr-Middleton@users.noreply.github.com>
2021-12-10 11:40:24 +0000

    added a script and a new cube
    changes have been made

---
+++ Collaborative-Sandbox/Assets/StarterAssets/ThirdPersonController/Scenes/Scripts/hopeitworks.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hopeitworks : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //lets pray
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
