b1acc032efeed21f9f70ab43a203a29f1e809f04
shedtronic <38550478+shedtronic@users.noreply.github.com>
2021-12-10 10:42:08 +0000

    Added two more boxes and testing script
    Added two more boxes and testing script

---
+++ Collaborative-Sandbox/Assets/Scripts/newTestingScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class newTestingScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Made a change to newTestingScript
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
